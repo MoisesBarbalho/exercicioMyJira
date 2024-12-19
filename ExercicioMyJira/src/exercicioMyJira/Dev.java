dacaeb301bc557f60654e205387bddc9939302eb
Particular <Particular@DESKTOP-85EBQGN>
2024-12-19 16:48:22 -0300

    metodos addDev e removeDev

--- ExercicioMyJira/src/exercicioMyJira/Dev.java
+++ ExercicioMyJira/src/exercicioMyJira/Dev.java
@@ -6,4 +6,10 @@ public class Dev {
 	private String nome;
 	private String time;
 	private ArrayList<Task> tarefas;
+	
+	public Dev(String nome, String time) {
+		this.nome = nome;
+		this.time = time;
+	}
+	
 }
--- ExercicioMyJira/src/exercicioMyJira/MyJira.java
+++ ExercicioMyJira/src/exercicioMyJira/MyJira.java
@@ -6,4 +6,22 @@ public class MyJira {
 	private String projeto;
 	private ArrayList<Task> backlog;
 	private ArrayList<Dev> equipe;
+	
+	public MyJira(String projeto) {
+		this.projeto = projeto;
+	}
+	
+	public void addDev(String nome, String time) {
+		Dev novoDev = new Dev(nome, time);
+		this.equipe.add(novoDev);
+	}
+	
+	public void removeDev(String nome, String time) {
+		Dev demitido = new Dev(nome, time);
+		for (Dev dev : this.equipe) {
+			if (demitido.equals(dev)) {
+				this.equipe.remove(dev);
+			}
+		}
+	}
 }
